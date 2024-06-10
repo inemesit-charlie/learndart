4979038641795d5f70fdf58fef941c0f2d4aebff
inemesit-charlie <inemesitcharlie@yahoo.com>
2024-06-10 16:07:39 +0100

    :

---
+++ typeannotation.dart
@@ -0,0 +1,17 @@
+void main() {
+ const String name = "Inemesit";
+    print(name);
+  const int age = 25;
+  print(age);
+
+  bool isOpen = true;
+   print(isOpen); 
+
+  double averageRating = 25.3;
+    averageRating = 29.89;
+    print(averageRating);
+
+//for writing null codebase where you intend to add values later you use a question mark "?" in front of the variable type 
+  int? point;
+  print(point);
+}
