df40eb3f86323724d59b8979a87ae06555476fee
inemesit-charlie <inemesitcharlie@yahoo.com>
2024-06-11 10:54:04 +0100

    List Class

---
+++ lists&sets.dart
@@ -0,0 +1,14 @@
+void main() {
+
+   List<int> scores = [50, 75, 30, 99];
+
+   //scores[1]=40;
+   scores.add(30); 
+   scores.removeLast();
+   //print(scores.length);
+   //print(scores.indexOf(99));
+   print(scores);
+
+Set<String> names = {"Inemesit", "Divine", "David", "Divine"};
+  print(names.length);
+}
