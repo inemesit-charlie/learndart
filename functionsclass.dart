3515f854622826cd08090e5c870e7d53bfd754aa
inemesit-charlie <inemesitcharlie@yahoo.com>
2024-06-11 09:16:11 +0100

    comments added

--- functionsclass.dart
+++ functionsclass.dart
@@ -1,3 +1,6 @@
+
+//the function in line 7 is a named parameter used to define the argument in line 4
+//- this is mainly used in cases where there are multiple complex arguments using the same type annotations but with diverse
 void main(){
 final greeting = greet(name: "Inemesit", age: 20 );
   print(greeting);
