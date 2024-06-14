5123d6cd747dc2b664bfb2586206df8c1b05d21a
inemesit-charlie <inemesitcharlie@yahoo.com>
2024-06-14 16:04:40 +0100

    Constructor Creation class

--- classescrashcourse/classescrashcourseeg1.dart
+++ classescrashcourse/classescrashcourseeg1.dart
@@ -7,10 +7,10 @@ void main() {
  
 class Person {
   //line 10 and line 11 are both properties of the class Person  
-  final String name;
+    final String name;
    final int age;
 //below is an example of a constructor
-  Person({
+  const Person({
         required this.name,
         required this.age
         
--- classescrashcourse/classescrashcourseeg2.dart
+++ classescrashcourse/classescrashcourseeg2.dart
@@ -1,6 +1,47 @@
 void main() {
+  const me = Person("Inemesit", 20);
+    print(me.name);
+    print(me.age);
+    print('--------------------');
 
+  const david = Person.david();
+    print(david.name);
+    print(david.age);
+    print('--------------------');
 
+  const bar = Person.bar(40);
+    print(bar.name);
+    print(bar.age);
+    print('--------------------');
 
-  
+  const others = Person.other();
+    print(others.name);
+    print(others.age); 
+    
+
+}
+
+class Person {
+    final String name;
+   final int age;
+
+  const Person(
+        this.name,
+        this.age
+  );
+//to create a new constructor with an initializer list
+  const Person.david()
+  : name = "David",
+    age = 21;
+
+  const Person.bar(this.age)
+  : name = "Bar";
+
+const Person.other({
+  String? name,
+  int? age,
+
+}) : name = name?? "Baz",
+     age = age?? 40;
+
 }
