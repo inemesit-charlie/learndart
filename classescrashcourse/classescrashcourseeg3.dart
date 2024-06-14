b8579db50ef38003f4d41cefdab5fe4dadb1ec9e
inemesit-charlie <inemesitcharlie@yahoo.com>
2024-06-14 17:11:06 +0100

    SubClasses

---
+++ classescrashcourse/classescrashcourseeg3.dart
@@ -0,0 +1,34 @@
+void main()  {
+    final v = Vehicle(4);
+    print(v);
+    print(v.toString());
+
+    print(Car());
+    print(Bicycle());
+}
+
+
+class Vehicle {
+    final int WheelCount;
+    const Vehicle(this.WheelCount);
+
+@override
+String toString() {
+
+ if(runtimeType == Vehicle) {  
+    return "Vehicle with $WheelCount wheels";
+    } else {
+      return super.toString();
+    }
+    }
+  }
+  
+
+class Car extends Vehicle {
+  const Car() : super(4);
+}
+
+class Bicycle extends Vehicle {
+  const Bicycle() : super(2);
+
+}
